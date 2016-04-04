6fff73d3f8b3d08f3f63ffad0f4f5c3da06e36ff
Rafael Nascimento Bezerra <rafaelbezerra.dev@gmail.com>
2016-04-04 01:15:23 -0400

    cleaning hw #7

--- hw/assignment_07/Rafael_Nascimento_Bezerra/ChatServerWebServer/app/src/main/java/edu/stevens/cs522/chat/oneway/server/managers/IQueryListener.java
+++
@@ -1,9 +0,0 @@
-package edu.stevens.cs522.chat.oneway.server.managers;
-
-/**
- * Created by Rafael on 2/21/2016.
- */
-public interface IQueryListener<T> {
-    public void handleResults(TypedCursor<T> results);
-    public void closeResults();
-}
