47b628c70946d40e9b873b406856b37e97a3ec59
Rafael Nascimento Bezerra <rafaelbezerra.dev@gmail.com>
2016-04-04 01:14:27 -0400

    hw #7 completed

---
+++ hw/assignment_07/Rafael_Nascimento_Bezerra/ChatServerWebServer/app/src/main/java/edu/stevens/cs522/chat/oneway/server/requests/Request.java
@@ -0,0 +1,62 @@
+package edu.stevens.cs522.chat.oneway.server.requests;
+
+import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.JsonReader;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import edu.stevens.cs522.chat.oneway.server.utils.App;
+
+/**
+ * Created by Rafael on 3/12/2016.
+ */
+public abstract class Request implements Parcelable {
+
+    public long clientID;
+    public UUID registrationID; // sanity check
+    protected Map<String, String> headers;
+    protected String requestEntity;
+
+    public Request() {
+        headers = new HashMap<>();
+        headers.put("X-latitude", "40.7439905");
+        headers.put("X-longitude", "-74.0323626");
+    }
+
+    protected Request(Parcel in) {
+        this();
+        requestEntity = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(requestEntity);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    // App-specific HTTP request headers.
+    public abstract Map<String, String> getRequestHeaders();
+
+    // Chat service URI with parameters e.g. query string parameters.
+    public abstract Uri getRequestUri();
+
+    // JSON body (if not null) for request data not passed in headers.
+    public abstract String getRequestEntity() throws IOException;
+
+    // Define your own Response class, including HTTP response code.
+    public abstract Response getResponse(HttpURLConnection connection, JsonReader rd /* Null for streaming */);
+
+    public UUID getRegistrationID() {
+        return registrationID;
+    }
+}
---
+++ hw/assignment_07/Rafael_Nascimento_Bezerra/ChatServerWebServer/app/src/main/java/edu/stevens/cs522/chat/oneway/server/utils/App.java
@@ -0,0 +1,26 @@
+package edu.stevens.cs522.chat.oneway.server.utils;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import edu.stevens.cs522.chat.oneway.server.activities.PreferencesActivity;
+
+/**
+ * Created by Rafael on 3/13/2016.
+ */
+public class App {
+    public static final String PREF_KEY_USERID = "userid";
+    public static final String PREF_KEY_LAST_SEQNUM = "last_seq_num";
+    public static final String PREF_KEY_USERNAME = "username";
+    public static final String PREF_KEY_HOST = "host";
+    public static final String PREF_KEY_REGISTRATION_ID = "reg_id";
+
+    final static public long PREF_DEFAULT_USER_ID = 0;
+    final static public long PREF_DEFAULT_LAST_SEQNUM = 0;
+    final static public String PREF_DEFAULT_USER_NAME = "no name";
+
+
+    public static String DEFAULT_HOST = "";
+    public static final String DEFAULT_ENCODING = "UTF-8";
+
+}
